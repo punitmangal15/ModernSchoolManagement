3bb4120eaae4de0823cbfbf912c7da3db41008ce
punitmangal15 <punitmangal15@gmail.com>
2025-09-23 11:47:21 +0530

    commit all

---
+++ ModernSchoolManagement.Dam/Models/ResponseModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModernSchoolManagement.Dam.Models
+{
+    public class ResponseModel<T>
+    {
+        public int ReturnCode { get; set; }
+        public string ReturnMessage { get; set; } = string.Empty;
+        public T? Content { get; set; }
+    }
+}
